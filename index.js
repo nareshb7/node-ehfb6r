5d0f0ea912c560503651d7962669f7c63b52e58b
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2022-12-23 10:59:11 +0530

    fs added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // run `node index.js` in the terminal
-
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
 console.log(`Hello Node.js v${process.versions.node}!`);
