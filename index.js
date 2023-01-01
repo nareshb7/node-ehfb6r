fc0f184306c0c780874e8f5a8420442324322c69
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2023-01-01 16:36:21 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // run `node index.js` in the terminal
 var http = require('http');
 var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+http
+  .createServer(function (req, res) {
+    fs.readFile('demofile1.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
+console.log('SERVER IS RUNNING...');
 console.log(`Hello Node.js v${process.versions.node}!`);
