9dc53fae81b3629d83f08fd7a68da9dd6409b75b
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2023-01-09 23:28:20 +0530

    Server Created

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // run `node index.js` in the terminal
 var http = require('http');
 var fs = require('fs');
-const arr =[1,2,3,4,5,6,7,8]
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+http
+  .createServer((req, res) => {
+    res.write('Hello World!!');
+    res.end();
+  })
+  .listen(4040, () => console.log('server is running....'));
 console.log('server is running...');
 fs.appendFile('test.txt', 'The AppendFile', (err, data) => {
   console.log('hello world', data, err);
